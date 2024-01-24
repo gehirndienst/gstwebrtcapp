6825701ac64a027e424c4c45e75d5b7a79442d8f
Nikita Smirnov <nsm@informatik.uni-kiel.de>
2024-01-24 15:27:48 +0000

    improved metrics for csv recording agent

--- gstwebrtcapp/control/recorder/agent.py
+++ gstwebrtcapp/control/recorder/agent.py
@@ -11,7 +11,8 @@ from utils.base import LOGGER
 from utils.gst import GstWebRTCStatsType, find_stat, get_stat_diff
 from utils.webrtc import clock_units_to_seconds, ntp_short_format_to_seconds
 
-class BrowserRecorderAgent(Agent):
+
+class CsvBrowserRecorderAgent(Agent):
     def __init__(
         self,
         controller: Controller,
@@ -26,13 +27,13 @@ class BrowserRecorderAgent(Agent):
         self.log_path = log_path
         self.verbose = min(verbose, 2)
         self.type = AgentType.RECORDER
-        
+
         self.stats = deque(maxlen=10000)
         self.last_stats = None
-        
+
         self.csv_handler = None
         self.csv_writer = None
-        
+
         self.is_running = False
 
     def run(self, _) -> None:
@@ -49,7 +50,7 @@ class BrowserRecorderAgent(Agent):
                         LOGGER.info(f"INFO: Browser Recorder agent stats:\n {self.stats[-1]}")
                     elif self.verbose == 2:
                         self._save_stats_to_csv()
-        
+
     def _fetch_stats(self) -> Dict[str, Any] | None:
         time.sleep(self.stats_update_interval)
         ticks = 0
@@ -63,31 +64,57 @@ class BrowserRecorderAgent(Agent):
             else:
                 gst_stats = self.controller.get_observation()
         return gst_stats
-    
+
     def _select_stats(self, gst_stats: Dict[str, Any]) -> bool:
         rtp_outbound = find_stat(gst_stats, GstWebRTCStatsType.RTP_OUTBOUND_STREAM)
         rtp_remote_inbound = find_stat(gst_stats, GstWebRTCStatsType.RTP_REMOTE_INBOUND_STREAM)
         if rtp_outbound is None or rtp_remote_inbound is None:
             LOGGER.info("WARNING: No RTP outbound or remote inbound stats were found in the GStreamer stats...")
             return False
-        
-        # fraction rx rate in Mbits
+
+        # last stats
         last_rtp_outbound = (
             find_stat(self.last_stats, GstWebRTCStatsType.RTP_OUTBOUND_STREAM) if self.last_stats is not None else None
         )
+        last_rtp_remote_inbound = (
+            find_stat(self.last_stats, GstWebRTCStatsType.RTP_REMOTE_INBOUND_STREAM)
+            if self.last_stats is not None
+            else None
+        )
+
+        # loss rate
+        loss_rate = (
+            float(rtp_remote_inbound["rb-packetslost"]) / rtp_outbound["packets-sent"]
+            if rtp_outbound["packets-sent"] > 0
+            else 0.0
+        )
+
+        # fraction rx rate in Mbits
         ts_diff_sec = get_stat_diff(rtp_outbound, last_rtp_outbound, "timestamp") / 1000
         rx_bytes_diff = get_stat_diff(rtp_outbound, last_rtp_outbound, "bytes-received")
         rx_mbits_diff = rx_bytes_diff * 8 / 1000000
-        rx_rate = rx_mbits_diff / ts_diff_sec  if ts_diff_sec > 0 else 0.0
-        
+        rx_rate = rx_mbits_diff / ts_diff_sec if ts_diff_sec > 0 else 0.0
+
+        # rtts / jitter
+        rtt_ms = ntp_short_format_to_seconds(rtp_remote_inbound["rb-round-trip"]) * 1000
+        last_rtt_ms = (
+            ntp_short_format_to_seconds(last_rtp_remote_inbound["rb-round-trip"]) * 1000
+            if last_rtp_remote_inbound is not None
+            else 0.0
+        )
+        gradient_rtt_ms = rtt_ms - last_rtt_ms
+        jitter_ms = clock_units_to_seconds(rtp_remote_inbound["rb-jitter"], rtp_outbound["clock-rate"]) * 1000
+
         # opened to extensions
         final_stats = {
-            "timestamp": time.strftime("%Y-%m-%d-%H:%M:%S:%f")[:-3],
-            "rr_fraction_loss": rtp_remote_inbound["rb-fractionlost"],
-            "rr_packets_lost": rtp_remote_inbound["rb-packetslost"],
-            "rr_ext_highest_seq": rtp_remote_inbound["rb-exthighestseq"],
-            "rr_jitter_ms": clock_units_to_seconds(rtp_remote_inbound["rb-jitter"], rtp_outbound["clock-rate"]) * 1000,
-            "rr_rtt_ms": ntp_short_format_to_seconds(rtp_remote_inbound["rb-round-trip"]) * 1000,
+            "timestamp": time.strftime("%Y-%m-%d-%H:%M:%S:%f"),
+            "fraction_packets_lost": rtp_remote_inbound["rb-fractionlost"],
+            "packets_lost": rtp_remote_inbound["rb-packetslost"],
+            "loss_rate_%": loss_rate,
+            "ext_highest_seq": rtp_remote_inbound["rb-exthighestseq"],
+            "rtt_ms": rtt_ms,
+            "gradient_rtt_ms": gradient_rtt_ms,
+            "jitter_ms": jitter_ms,
             "nack_count": rtp_outbound["recv-nack-count"],
             "pli_count": rtp_outbound["recv-pli-count"],
             "rx_packets": rtp_outbound["packets-received"],
@@ -95,11 +122,12 @@ class BrowserRecorderAgent(Agent):
             "tx_rate_mbits": rtp_outbound["bitrate"] / 1000000,
             "rx_rate_mbits": rx_rate,
         }
-        
+        print(final_stats)
+
         self.stats.append(final_stats)
         self.last_stats = gst_stats
         return True
-    
+
     def _save_stats_to_csv(self) -> None:
         if self.csv_handler is None:
             datetime_now = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
@@ -113,7 +141,6 @@ class BrowserRecorderAgent(Agent):
             self.csv_handler.flush()
         else:
             self.csv_writer.writerow(self.stats[-1])
-        
 
     def stop(self) -> None:
         LOGGER.info("INFO: stopping Browser Recorder agent...")
