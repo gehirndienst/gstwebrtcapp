fa4a3bde138209d96026a3905309c35fdb1c4335
Nikita Smirnov <nsm@informatik.uni-kiel.de>
2024-02-12 08:37:28 +0000

    updated pipelines

--- gstwebrtcapp/apps/pipelines.py
+++ gstwebrtcapp/apps/pipelines.py
@@ -16,7 +16,7 @@ DEFAULT_BIN_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 !
     x264enc name=encoder tune=zerolatency speed-preset=superfast threads=4 key-int-max=2560 b-adapt=false vbv-buf-capacity=120 ! 
-    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1380 !
+    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1370 !
     capsfilter name=payloader_capsfilter caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)126" ! webrtc.
 '''
 # for internal reolink camera that streams 4k in hevc
@@ -25,16 +25,16 @@ DEFAULT_H265_IN_WEBRTCBIN_H264_OUT_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 ! 
     x264enc name=encoder tune=zerolatency speed-preset=superfast threads=8 key-int-max=2560 b-adapt=false vbv-buf-capacity=120 !
-    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1380 ! 
+    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1370 ! 
     capsfilter name=payloader_capsfilter caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)126, rtcp-fb-goog-remb=(boolean)true, rtcp-fb-transport-cc=(boolean)true" ! webrtc.
 '''
 
 DEFAULT_H265_IN_WEBRTCBIN_H264_OUT_CUDA_PIPELINE = '''
     webrtcbin name=webrtc latency=1 bundle-policy=max-bundle stun-server=stun://stun.l.google.com:19302
-    rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert ! videoscale ! videorate ! cudaupload ! cudaconvert ! 
+    rtspsrc name=source location=rtsp://10.10.3.254:554 ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert ! videoscale ! videorate ! cudaupload ! cudaconvert ! 
     capsfilter name=raw_capsfilter caps=video/x-raw(memory:CUDAMemory) ! 
     nvh264enc name=encoder preset=low-latency-hq gop-size=2560 rc-mode=cbr-ld-hq zerolatency=true ! 
-    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1380 !  
+    rtph264pay name=payloader auto-header-extension=true config-interval=1 mtu=1370 ! 
     capsfilter name=payloader_capsfilter caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)126" ! webrtc.
 '''
 # NOTE: Chrome does not play HEVC, a black screen
@@ -43,7 +43,7 @@ DEFAULT_WEBRTCBIN_H265_OUT_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 ! 
     x265enc name=encoder key-int-max=2560 speed-preset=superfast tune=zerolatency ! h265parse ! 
-    rtph265pay name=payloader auto-header-extension=true config-interval=1 mtu=1380 ! 
+    rtph265pay name=payloader auto-header-extension=true config-interval=1 mtu=1370 ! 
     capsfilter name=payloader_capsfilter ! webrtc.
 '''
 
@@ -52,7 +52,7 @@ DEFAULT_WEBRTCBIN_H265_OUT_CUDA_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate ! cudaupload ! cudaconvert !
     capsfilter name=raw_capsfilter caps=video/x-raw(memory:CUDAMemory),format=I420 ! 
     nvh265enc name=encoder preset=low-latency-hq gop-size=2560 rc-mode=cbr-ld-hq zerolatency=true ! h265parse ! 
-    rtph265pay name=payloader auto-header-extension=true config-interval=1 mtu=1380 ! 
+    rtph265pay name=payloader auto-header-extension=true config-interval=1 mtu=1370 ! 
     capsfilter name=payloader_capsfilter ! webrtc.
 '''
 
@@ -62,7 +62,7 @@ DEFAULT_WEBRTCBIN_VP8_OUT_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 ! 
     vp8enc name=encoder deadline=1 keyframe-max-dist=2000 keyframe-mode=disabled threads=8 cpu-used=4 ! 
-    rtpvp8pay name=payloader auto-header-extension=true picture-id-mode=15-bit mtu=1380 ! 
+    rtpvp8pay name=payloader auto-header-extension=true picture-id-mode=15-bit mtu=1370 ! 
     capsfilter name=payloader_capsfilter ! webrtc.
 '''
 
@@ -72,7 +72,7 @@ DEFAULT_WEBRTCBIN_VP9_OUT_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 ! 
     vp9enc name=encoder deadline=1 keyframe-max-dist=2000 keyframe-mode=disabled row-mt=true threads=8 cpu-used=4 ! 
-    rtpvp9pay name=payloader auto-header-extension=true picture-id-mode=15-bit mtu=1380 ! 
+    rtpvp9pay name=payloader auto-header-extension=true picture-id-mode=15-bit mtu=1370 ! 
     capsfilter name=payloader_capsfilter ! webrtc.
 '''
 
@@ -82,7 +82,7 @@ DEFAULT_WEBRTCBIN_AV1_OUT_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 !  rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate !
     capsfilter name=raw_capsfilter caps=video/x-raw,format=I420 ! 
     av1enc name=encoder usage-profile=realtime qos=true threads=8 cpu-used=4 ! av1parse !
-    rtpav1pay name=payloader auto-header-extension=true mtu=1380 ! 
+    rtpav1pay name=payloader auto-header-extension=true mtu=1370 ! 
     capsfilter name=payloader_capsfilter ! webrtc.
 '''
 
@@ -91,7 +91,7 @@ DEFAULT_BIN_CUDA_PIPELINE = '''
     rtspsrc name=source location=rtsp://10.10.3.254:554 latency=10 !  rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! videorate ! cudaupload ! cudaconvert ! 
     capsfilter name=raw_capsfilter caps=video/x-raw(memory:CUDAMemory) ! 
     nvh264enc name=encoder preset=low-latency-hq gop-size=2560 rc-mode=cbr-ld-hq zerolatency=true ! 
-    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1380 ! 
+    rtph264pay name=payloader auto-header-extension=true aggregate-mode=zero-latency config-interval=1 mtu=1370 ! 
     capsfilter name=payloader_capsfilter caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)126" ! webrtc.
 '''
 
