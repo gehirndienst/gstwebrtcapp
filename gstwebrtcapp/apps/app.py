8974b762b890b6800ff499911148714ac9dcfb07
Nikita Smirnov <nsm@informatik.uni-kiel.de>
2024-04-09 07:28:03 +0000

    fixed hardcoded nvenc encoder

--- gstwebrtcapp/apps/app.py
+++ gstwebrtcapp/apps/app.py
@@ -81,15 +81,6 @@ class GstWebRTCApp(metaclass=ABCMeta):
         self.video_url = config.video_url
         self.encoder_gst_name = get_gst_encoder_name(config.codec, config.is_cuda)
         self.is_cuda = config.is_cuda and config.codec.startswith('h26')  # FIXME: so far only h264/h265 are supported
-        if self.is_cuda:
-            # FIXME: add support for inserting cudaupload and cudaconvert into the pipeline
-            pattern = re.compile(r'(!\s*.*?name=encoder.*?!)')
-            replacement_line = (
-                '! nvh264enc name=encoder preset=low-latency-hq gop-size=2560 rc-mode=cbr-ld-hq zerolatency=true !'
-                if config.codec == 'h264'
-                else '! nvh265enc name=encoder preset=low-latency-hq gop-size=2560 rc-mode=cbr-ld-hq zerolatency=true !'
-            )
-            self.pipeline_str = pattern.sub(replacement_line, self.pipeline_str)
 
         self.bitrate = config.bitrate
         self.resolution = config.resolution
